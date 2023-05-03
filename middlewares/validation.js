f7aa8807cd411c10ccae00c96a8bb682bafc88de
Liza Iutina <iutinaelizaveta@yandex.ru>
2023-05-03 23:31:39 +0300

    fix: for test

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -4,7 +4,7 @@ const { regExp } = require('../utils/constants');
 
 const validateGetUser = celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().hex().required(),
+    userId: Joi.string().hex().length(24).required(),
   }),
 });
 
